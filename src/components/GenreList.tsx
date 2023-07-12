8a02e160e5c8aee3204f937d562c0f6df5791c61
dominiKaczmarek <kaczmarek.dominik97@gmail.com>
2023-07-12 08:07:45 +0200

    Adding genre list skeletons

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,18 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import GenreListSkeleton from "./GenreListSkeleton";
 
 function GenreList() {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  if (error) return null;
+
   return (
     <List>
+      {isLoading &&
+        skeletons.map((skeleton) => <GenreListSkeleton key={skeleton} />)}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="4px">
           <HStack>
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,21 @@
+import {
+  Card,
+  CardBody,
+  HStack,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+} from "@chakra-ui/react";
+
+function GenreListSkeleton() {
+  return (
+    <Card height="32px" borderRadius={10} overflow="hidden" marginY="8px">
+      <Skeleton></Skeleton>
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+}
+
+export default GenreListSkeleton;
